f027b0d461f2135f579f37d606d52be95f0dfecc
atharvpatil18 <atharvnpatil18@gmail.com>
2026-06-13 13:46:21 +0530

    build: clean up scratch files, scraper outputs, sitemaps, and update gitignore

--- backend/scratch/get_build_id.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("next_data.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-print("Build ID:", data.get("buildId"))
-print("Locale:", data.get("locale"))
-print("Query:", data.get("query"))
